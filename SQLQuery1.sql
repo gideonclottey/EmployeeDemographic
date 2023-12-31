db7c9741fda9ee27e56d5ed35a2edf8a830eba2e
gideonclottey <gideonclotteyint@gmail.com>
2023-12-31 19:17:52 +0000

    Creating the Employee salary table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,5 @@
+USE SQLTutorial
+
 CREATE TABLE EmployeeDemographics
 (EmployyeId int,
 FirstName Varchar(50),
@@ -5,3 +7,8 @@ LastName Varchar(50),
 Age int,
 Gender Varchar(50)
 )
+
+CREATE TABLE EmployeeSalary
+(EmployeeId int,
+JobTitle Varchar(50),
+salary int)
